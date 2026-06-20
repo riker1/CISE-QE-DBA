865b1b20f4d2893b5fde380d4d79898f5c4de1a0
Eric S. Tyrer II <etyrer@york.cuny.edu>
2026-06-20 00:54:49 -0400

    no need to track

--- debug_import_stack.py
+++
@@ -1,9 +0,0 @@
-import faulthandler
-import sys
-
-faulthandler.enable()
-faulthandler.dump_traceback_later(60, repeat=True, file=sys.stderr)
-
-print("starting Util import", flush=True)
-from python_files.Util import *
-print("Util import finished", flush=True)
