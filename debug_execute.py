a426c84993b29ab5c3542f8259b431dab3543fe8
Eric S. Tyrer II <etyrer@york.cuny.edu>
2026-06-19 21:00:00 -0400

    refactor: add faulthandler setup for improved debugging in notebook execution

--- debug_execute.py
+++ debug_execute.py
@@ -2,6 +2,7 @@ import nbformat
 from nbclient import NotebookClient
 
 nb = nbformat.read("Demo.ipynb", as_version=4)
+
 client = NotebookClient(
     nb,
     timeout=-1,
@@ -9,12 +10,22 @@ client = NotebookClient(
     kernel_name="python3",
 )
 
+debug_cell = nbformat.v4.new_code_cell("""
+import faulthandler, sys
+faulthandler.enable()
+faulthandler.dump_traceback_later(60, repeat=True, file=sys.__stderr__)
+print("faulthandler enabled")
+""")
+
 with client.setup_kernel():
+    print("--- Running debug setup cell ---", flush=True)
+    client.execute_cell(debug_cell, -1)
+
     for i, cell in enumerate(nb.cells):
         if cell.cell_type != "code":
             continue
 
-        print(f"\n--- Running cell {i} ---", flush=True)
+        print(f"\\n--- Running cell {i} ---", flush=True)
         print(cell.source[:500], flush=True)
 
         client.execute_cell(cell, i)
